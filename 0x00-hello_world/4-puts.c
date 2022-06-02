fc0133565ae0e501b2c1940409a7589e109c3046
Kimberlee <kunniahkimberlee@gmail.com>
2022-06-02 05:17:01 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/* 
- * main function
- * code
- * return -successful
-*/
+/**
+ *  main - start
+ *  
+ *  Return:0 (Success)
+ **/
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
