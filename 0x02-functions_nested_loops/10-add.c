5922f71533216319fe2ba7a364a039ff786ecac5
Kimberlee <kunniahkimberlee@gmail.com>
2022-06-07 09:04:56 -0700

    add a and b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - check description
+ * @a - input 1
+ * @b - input 2
+ * Description: adds two integers and returns the result
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
