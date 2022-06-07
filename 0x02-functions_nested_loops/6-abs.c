a9e94bd3c2d61c0303704d85c05cc60a4bff5069
Kimberlee <kunniahkimberlee@gmail.com>
2022-06-07 08:32:06 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - check description
+ * @r: input
+ * Description: fuction computes absolute value
+ * of a value
+ * Return: value
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
