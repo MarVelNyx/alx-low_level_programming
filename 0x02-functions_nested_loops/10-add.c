e5642b1cb8f0aa33858c5626deecff2c52328f5b
Kimberlee <kunniahkimberlee@gmail.com>
2022-06-07 09:06:29 -0700

    add a and b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - check description
- * @a - input 1
- * @b - input 2
+ * @a - integer a
+ * @b - integer b
  * Description: adds two integers and returns the result
  * Return: sum of a and b
  */
