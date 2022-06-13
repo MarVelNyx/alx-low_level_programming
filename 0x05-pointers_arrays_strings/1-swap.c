13d5aedc60cce95dd2615b7edaa8687d27eb4e48
Kimberlee <kunniahkimberlee@gmail.com>
2022-06-13 04:58:47 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int - function that swaps the values of 
+ * swap_int - function that swaps the values of
  * two integers
  * @a: input int pointer
  * @b: input int pointer
