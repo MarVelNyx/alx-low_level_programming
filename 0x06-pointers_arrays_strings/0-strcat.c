cd9faa453a2f4cd71a1127815de4232c625ed4c6
Kimberlee <kunniahkimberlee@gmail.com>
2022-06-15 08:26:08 -0700

    removing swp file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,4 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: input string
- * @src: input string
- * Return: dest
- */
-
-char *_strcat(char *dest, char *src)
-{
-	char *tmp = dest;
-
-	while (*dest)
-	{
-		dest++;
-	}
-
-	while (*src)
-	{
-		*dest++ = *src++;
-	}
-	*dest = '\0';
-	return (tmp);
-}
+ * 
