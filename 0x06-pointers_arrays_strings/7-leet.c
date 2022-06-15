8affed5d81c21289f25dd74ff97f7b41a974eb7a
Kimberlee <kunniahkimberlee@gmail.com>
2022-06-15 10:02:22 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * @s: input string
+ * Return: encoded string
+ */
+
+char *leet(char *s)
+{
+	int j, i = 0;
+	char sub[10] = { '4', '4', '3', '3', '0', '0', '7', '7' '1', '1' };
+	char letter[10] = { 'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L' };
+
+	while (s[i])
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == letter[j])
+				s[i] = sub[j];
+		}
+		i++;
+	}
+	return (s);
+}
