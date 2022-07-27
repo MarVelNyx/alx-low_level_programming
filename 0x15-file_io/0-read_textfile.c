c511cfa0e89e031b2ec100de15a345699128ac4a
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-07-27 04:16:39 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcnt1.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - functiion reads text file and prints
+ * to POSIX STDOUT
+ * @filename: filename to open
+ * @letters: num of letters to read and print
+ * Return: number of letters read and printed/ 0 = fail
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fdo, fdr, fdw;
+	char *temp;
+
+	if (filename == NULL)
+		return (0);
+
+	temp = malloc(sizeof(char) * letters);
+	if (temp == NULL)
+		return (0);
+
+	fdo = open(filename, O_RDONLY);
+	if (fdo < 0)
+	{
+		free(temp);
+		return (0);
+	}
+
+	fdr = read(fdo, temp, letters);
+	if (fdr < 0)
+	{
+		free(temp);
+		return (0);
+	}
+
+	fdw = write(STDOUT_FILENO, temp, fdr);
+	free(temp);
+	close(fdo);
+
+	if (fdw < 0)
+		return (0);
+	return ((ssize_t)fdw);
+}
