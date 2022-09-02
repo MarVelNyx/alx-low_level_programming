4fb9ea2e3570a7c575aaf545c72dfcd23193ac9a
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-09-02 09:52:14 -0700

    Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists"
+
+/**
+ * free_dlistint -  function that frees a dlistint_t list
+ * @head: pointer to head
+ * Return: Nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
