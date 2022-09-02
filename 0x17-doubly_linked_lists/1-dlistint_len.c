299d229c0b2d8b95a4e63682eaf353962ce8f150
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-09-02 09:29:33 -0700

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that returns the number of elements
+ * in a linked dlistint_t list
+ * @h: head of list
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+	const dlistint_t *temp;
+
+	tenp = h;
+	while (temp)
+	{
+		temp = temp->next;
+		nodes++;
+	}
+	return (nodes);
+}
