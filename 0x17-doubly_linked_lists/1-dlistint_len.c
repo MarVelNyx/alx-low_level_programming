21923aff0ec64082a4d667a1747fb6ecd7291f5a
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-09-02 09:32:16 -0700

    Task 1 update

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,12 +10,14 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes = 0;
 	const dlistint_t *temp;
+	int count = 0;
 
 	tenp = h;
 	while (temp)
 	{
 		temp = temp->next;
 		nodes++;
+		count++;
 	}
-	return (nodes);
+	return (count);
 }
